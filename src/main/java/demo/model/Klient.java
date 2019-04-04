e32efe07989f126c1f815470892ca66f87d7be20
tomaszhejak <tomasz.hejak@gmail.com>
2019-04-04 21:24:01 +0200

    tesr

--- src/main/java/demo/model/Klient.java
+++ src/main/java/demo/model/Klient.java
@@ -4,24 +4,18 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Cacheable;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.hibernate.annotations.Cache;
-import org.springframework.cache.annotation.Caching;
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 
 @Entity(name = "klient")
 @Table(schema = "public")
